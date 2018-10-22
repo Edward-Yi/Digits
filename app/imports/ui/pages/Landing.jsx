f07fe33101158d83bc463c1f45746eadc0fae280
Edward Yi <42788217+Edward-Yi@users.noreply.github.com>
2018-10-22 13:19:29 -1000

    landing page mockup finished in 17 minutes

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,22 +1,32 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Icon, Header } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-        <Grid verticalAlign='middle' textAlign='center' container>
+        <div className='landing-background'>
+          <Grid container centered stackable columns={3}>
 
-          <Grid.Column width={4}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
-          </Grid.Column>
+            <Grid.Column textAlign='center'>
+              <Icon inverted size='huge' name='address book outline'/>
+              <Header as='h1' inverted>Contact Users</Header>
+              <Header as='h3' inverted>Keep in touch with fellow users and future coworkers</Header>
+            </Grid.Column>
 
-          <Grid.Column width={8}>
-            <h1>Welcome to this template</h1>
-            <p>Now get to work and modify this app!</p>
-          </Grid.Column>
+            <Grid.Column textAlign='center'>
+              <Icon inverted size='huge' name='folder outline'/>
+              <Header as='h1' inverted>Shared Storage</Header>
+              <Header as='h3' inverted>Store a project with other users you have been working with</Header>
+            </Grid.Column>
 
-        </Grid>
+            <Grid.Column textAlign='center'>
+              <Icon inverted size='huge' name='question circle outline'/>
+              <Header as='h1' inverted>Questions</Header>
+              <Header as='h3' inverted>Please contact us at: (1000)156-2222</Header>
+            </Grid.Column>
+          </Grid>
+        </div>
     );
   }
 }
